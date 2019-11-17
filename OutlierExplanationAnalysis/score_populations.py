d502e34a6b46ac219b226935ddbed90ddcce237b
myrtakis <myrtakis@csd.uoc.gr>
2019-11-17 13:47:10 +0200

    Datasets and Experiment Results

---
+++ OutlierExplanationAnalysis/score_populations.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+from sklearn.neighbors import LocalOutlierFactor
+import matplotlib.pyplot as plt
+
+rel_feature_subsets_col_name = 'subspaces'
+
+
+def analyze_score_populations(path_dataset, path_ground_truth, dirs_algos_outputs):
+
+    os.listdir()
+
+
+if __name__ == "__main__":
+    print('Score Populations')
