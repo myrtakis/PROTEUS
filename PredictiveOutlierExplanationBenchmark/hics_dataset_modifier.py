c782997eaaa95ebd624c69c88f2a3421bac413de
myrtakis <myrtakis@csd.uoc.gr>
2020-01-23 22:19:34 +0200

    New 5 hics subdatasets created

--- PredictiveOutlierExplanationBenchmark/hics_dataset_modifier.py
+++ PredictiveOutlierExplanationBenchmark/hics_dataset_modifier.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import pandas as pd
 import argparse
 import os
@@ -40,6 +42,18 @@ def find_outliers_to_keep(groups, subspace_map):
     return outlier_to_keep
 
 
+def find_subspaces_to_keep(groups, subspace_map):
+    subspaces_to_keep = []
+    for dim, subspaces in subspace_map.items():
+        counter = 0
+        for sid_pids in subspaces.items():
+            if counter == groups:
+                break
+            subspaces_to_keep.append(sid_pids)
+            counter += 1
+    return subspaces_to_keep
+
+
 def get_subspaces_and_indexes(df, outlier_indexes, min_dim, max_dim):
     outlier_df = df.iloc[outlier_indexes, :]
     subspace_map = {}
@@ -70,9 +84,67 @@ def get_files(dir_path):
 
 def create_dim_exp_from_dataset(args):
     df = pd.read_csv(args.main_file)
+    numeric_features = df.shape[1] - 2
     outlier_indexes = np.where(df[is_anomaly_column] == 1)[0]
     sub_map = get_subspaces_and_indexes(df, outlier_indexes, args.minimum_dimensionality, args.maximum_dimensionality)
-    print(sub_map)
+    subspaces_to_keep = find_subspaces_to_keep(args.groups, sub_map)
+    outliers_to_keep = set(chain.from_iterable([x[1] for x in subspaces_to_keep]))
+    outliers_to_remove = set(outlier_indexes) - outliers_to_keep
+    base_df = get_base_df_target_subspaces(df, subspaces_to_keep, outliers_to_remove)
+    target, subspaces = base_df[is_anomaly_column], base_df[subspace_column]
+    base_df = base_df.drop(columns=[is_anomaly_column, subspace_column])
+    df = df.drop(index=outliers_to_remove)
+    features_per_dataset = int(np.ceil(numeric_features / args.sub_datasets_num))
+    features_pool = get_features_pool_shuffled(subspaces_to_keep, numeric_features)
+    col_names = list(base_df.columns)
+    for i in range(args.sub_datasets_num):
+        features_to_add = (i+1) * features_per_dataset - base_df.shape[1]
+        if base_df.shape[1] + features_to_add > numeric_features:
+            features_to_add = numeric_features - base_df.shape[1]
+        col_names.extend(list(df.iloc[:, features_pool[0:features_to_add]].columns))
+        base_df = pd.concat([base_df, df.iloc[:, features_pool[0:features_to_add]]], ignore_index=True, axis=1)
+        base_df.columns = col_names
+        features_pool = np.delete(features_pool, list(range(features_to_add)))
+        dataset_name = 'hics_' + str(base_df.shape[1]) + '_g' + str(args.groups) \
+                       + '_dmin' + str(args.minimum_dimensionality) + '_dmax' + str(
+            args.maximum_dimensionality) + '.csv'
+        if not os.path.exists(args.save_dir):
+            os.makedirs(args.save_dir)
+        output_file = os.path.join(args.save_dir, dataset_name)
+        tmpdf = base_df.copy()
+        tmpdf[is_anomaly_column] = target
+        tmpdf[subspace_column] = subspaces
+        tmpdf.to_csv(output_file, index=False)
+
+
+def get_base_df_target_subspaces(df, subspaces_to_keep, outliers_to_remove):
+    base_df = pd.DataFrame()
+    col_names = []
+    target = df[is_anomaly_column]
+    subspaces = df[subspace_column].copy()
+    for item in subspaces_to_keep:
+        subspace_feautures = list(map(int, item[0].split()))
+        feature_sig = np.arange(base_df.shape[1], base_df.shape[1] + len(subspace_feautures))
+        subspaces.iloc[item[1]] = str(feature_sig) + ';'
+        tmpdf = df.iloc[:, subspace_feautures]
+        col_names.extend(list(tmpdf.columns))
+        base_df = pd.concat([base_df, tmpdf], ignore_index=True, axis=1)
+    base_df = pd.concat([base_df, target, subspaces], ignore_index=True, axis=1)
+    col_names.append(is_anomaly_column)
+    col_names.append(subspace_column)
+    base_df.columns = col_names
+    return base_df.drop(index=outliers_to_remove)
+
+
+def get_features_pool_shuffled(subspaces_to_keep, num_of_features):
+    feature_pool = set(np.arange(0, num_of_features))
+    for item in subspaces_to_keep:
+        for f in list(map(int, item[0].split())):
+            feature_pool.remove(f)
+    feature_pool = np.array(list(feature_pool))
+    np.random.seed(0)
+    np.random.shuffle(feature_pool)
+    return feature_pool
 
 
 if __name__ == '__main__':
