Predictive pipeline fixed, variance of noise reduced

PredictiveOutlierExplanationBenchmark/src/analysis/comparison/detector_comparison.py
@@ -10,12 +10,13 @@ import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 MAX_FEATURES = 10
 
 # conf = {'path': '../results_normal/lof', 'detector': 'lof', 'holdout': False, 'type': 'synthetic'}
-conf = {'path': '../results_normal/iforest', 'detector': 'lof', 'holdout': False, 'type': 'synthetic'}
-# conf = {'path': '../results_normal/lof', 'detector': 'lof', 'holdout': False, 'type': 'real'}
+# conf = {'path': '../results_normal/iforest', 'detector': 'iforest', 'holdout': False, 'type': 'synthetic'}
+conf = {'path': '../results_normal/lof', 'detector': 'lof', 'holdout': False, 'type': 'real'}
 
 
 def baseline_explanations(baseline_path):
@@ -30,10 +31,15 @@ def baseline_explanations(baseline_path):
 def compare_methods():
     nav_files_json = sort_files_by_dim(read_nav_files(conf['path'], conf['type']))
     explanation_perfs = []
-    dimensions = []
     methods = []
+    dataset_names = []
     for dim, nav_file in nav_files_json.items():
-        dimensions.append(dim-1-(conf['type'] == 'synthetic'))
+        real_dims = dim-1-(conf['type'] == 'synthetic')
+        dname = get_dataset_name(nav_file[FileKeys.navigator_original_dataset_path])
+        if conf['type'] == 'synthetic':
+            dataset_names.append('HiCS ' + str(real_dims) + '-d')
+        else:
+            dataset_names.append(dname + ' ' + str(real_dims) + '-d')
         ConfigMger.setup_configs(nav_file[FileKeys.navigator_conf_path])
         ps_mger = PseudoSamplesMger(nav_file[FileKeys.navigator_pseudo_samples_key], 'roc_auc', fs=True)
         detector = get_detector_model()
@@ -44,7 +50,7 @@ def compare_methods():
         if len(methods) == 0:
             methods = sorted(explanations.keys())
         explanation_perfs.append(auc_perfs_using_explanation(detector, dataset, explanations))
-    return pd.DataFrame(np.array(explanation_perfs).T, index=methods, columns=dimensions)
+    return pd.DataFrame(np.array(explanation_perfs).T, index=methods, columns=dataset_names)
 
 
 def auc_perfs_using_explanation(detector, dataset, explanations):
@@ -67,6 +73,10 @@ def get_detector_model():
     assert False
 
 
+def get_dataset_name(dataset_path):
+    return os.path.splitext(os.path.basename(dataset_path))[0]
+
+
 def get_dataset(ps_mger):
     dataset_path_det = ps_mger.get_dataset_path_of_k(0)
     return Dataset(dataset_path_det, DatasetConfig.get_anomaly_column_name(), DatasetConfig.get_subspace_column_name())
@@ -76,11 +86,9 @@ def visualize_results_as_table(perfomance_df):
     perfomance_df = perfomance_df.round(3)
     hold_out = 'yes' if conf['holdout'] else 'no'
     title = 'Explaining ' + conf['detector'].upper() + ' (hold out: ' + hold_out + ')'
-    if conf['type'] == 'synthetic':
-        col_names = ['HiCS ' + str(d) + '-d' for d in perfomance_df.columns]
     fig, ax = plt.subplots(nrows=1, ncols=1)
 
-    table = ax.table(cellText=perfomance_df.values, colLabels=col_names,
+    table = ax.table(cellText=perfomance_df.values, colLabels=perfomance_df.columns,
                      loc='top', rowLabels=perfomance_df.index,
                      cellLoc='center', bbox=[0.15, 0.45, 0.8, 0.5])
 

PredictiveOutlierExplanationBenchmark/src/pipeline/DatasetTransformer.py
@@ -82,8 +82,10 @@ class Transformer:
             outlier_row_pos = np.where(indexes == outlier)[0][0]
             o_sample = new_df.iloc[outlier_row_pos, :].values
             start_ps_ind = new_df.shape[0]
+            # print(list(o_sample))
             for ps_sample in range(pseudo_samples_per_outlier):
-                pseudo_sample = o_sample + Transformer.__gaussian_noise(dataset)
+                pseudo_sample = o_sample + Transformer.__gaussian_noise(dataset, o_sample)
+                # print('ps', list(pseudo_sample))
                 new_df = new_df.append(pd.Series(pseudo_sample[0], index=new_df.columns), ignore_index=True)
             pseudo_samples_inds_per_outlier[outlier_row_pos] = (start_ps_ind, new_df.shape[0])
         pseudo_samples_df = new_df.iloc[pseudo_samples_indices, :]
@@ -95,9 +97,9 @@ class Transformer:
         return new_dataset
 
     @staticmethod
-    def __gaussian_noise(dataset):
+    def __gaussian_noise(dataset, o_sample):    # o_sample to be potentially used a the center of the distribution
         noise = np.zeros(dataset.get_X().shape[1])
-        for i, (mu, sigma) in enumerate(zip(dataset.get_mean_per_column(), dataset.get_std_per_column())):
+        for i, (mu, sigma) in enumerate(zip(noise, 0.1 * dataset.get_std_per_column())):
             noise[i] = np.random.normal(mu, sigma)
         return noise
 

PredictiveOutlierExplanationBenchmark/src/pipeline/Detection.py
@@ -7,17 +7,18 @@ from PredictiveOutlierExplanationBenchmark.src.utils.metrics import calculate_ro
 
 
 def evaluate_detectors(dataset, detector_id):
+    threshold_percentage = len(dataset.get_outlier_indices()) / dataset.get_X().shape[0]
     detectors_arr = Detector.init_detectors()
-    detectors_info = select_detector(detectors_arr, dataset, detector_id)
+    detectors_info = select_detector(detectors_arr, dataset, detector_id, threshold_percentage)
     if SettingsConfig.is_classification_task():
-        new_dataset, threshold = create_dataset_classification(dataset, detectors_info['best'].get_scores_in_train())
+        new_dataset, threshold = create_dataset_classification(dataset, detectors_info['best'].get_scores_in_train(), threshold_percentage)
         return new_dataset, detectors_info, threshold
     else:
         new_dataset, threshold = create_dataset_regression(dataset, detectors_info['best'].get_scores_in_train())
         return new_dataset, detectors_info, threshold
 
 
-def select_detector(detectors_arr, dataset, detector_id_to_select):
+def select_detector(detectors_arr, dataset, detector_id_to_select, threshold_percentage):
     best_detector = None
     max_perf = None
     detectors_info = {'info': {}, 'best': None}
@@ -38,7 +39,7 @@ def select_detector(detectors_arr, dataset, detector_id_to_select):
         selected_detector = detectors_info['info'][detector_id_to_select]
     print('Selected detector:', selected_detector.get_id(), 'with auc score', selected_detector.get_effectiveness())
     desc_ordered_indices = np.argsort(selected_detector.get_scores_in_train())[::-1]
-    topk_points = floor(SettingsConfig.get_top_k_points_to_explain() * dataset.get_X().shape[0])
+    topk_points = floor(threshold_percentage * dataset.get_X().shape[0])
     true_outliers = set(desc_ordered_indices[:topk_points]).intersection(dataset.get_outlier_indices())
     print('True outliers found for threshold', SettingsConfig.get_top_k_points_to_explain(), ':', true_outliers)
     detectors_info['best'] = selected_detector
@@ -46,18 +47,18 @@ def select_detector(detectors_arr, dataset, detector_id_to_select):
 
 
 def detect(detector, X, Y):
+    threshold_percentage = len(np.where(Y == 1)[0]) / X.shape[0]
     scores = detector.predict(X)
     desc_ordered_indices = np.argsort(scores)[::-1]
-    topk = floor(SettingsConfig.get_top_k_points_to_explain() * X.shape[0])
+    topk = floor(threshold_percentage * X.shape[0])
     topk_points = desc_ordered_indices[:topk]
     labels = np.zeros(X.shape[0], dtype=int)
     labels[topk_points] = 1
     return labels, scores, calculate_roc_auc(Y, labels)
 
 
-def create_dataset_classification(dataset, scores):
-    top_k_points = SettingsConfig.get_top_k_points_to_explain(),
-    top_k_points_to_keep = floor(top_k_points[0] * dataset.get_X().shape[0])
+def create_dataset_classification(dataset, scores, threshold_percentage):
+    top_k_points_to_keep = floor(threshold_percentage * dataset.get_X().shape[0])
     desc_ordered_indices = np.argsort(scores)[::-1]
     desc_ordered_outlier_indices = desc_ordered_indices[range(top_k_points_to_keep)]
     lowest_outlier_score = scores[desc_ordered_outlier_indices[-1]]
@@ -87,3 +88,4 @@ def create_dataset_regression(dataset, scores):
 def assess_detector(y_pred, y_true):
     assert SettingsConfig.is_classification_task()
     return calculate_roc_auc(y_true, y_pred)
+